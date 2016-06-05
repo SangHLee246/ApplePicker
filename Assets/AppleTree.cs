0eb3bbbed07f3c20b41ebd730e849523b458154d
SangHLee246 <akayasha1234@yahoo.com>
2016-06-05 15:01:04 -0400

    Apple Tree Moves Randomly
    Added C# code to change the direction that the Apple Tree moves in.

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -20,5 +20,17 @@ public class AppleTree : MonoBehaviour {
 		Vector3 pos = transform.position;
 		pos.x += speed * Time.deltaTime;
 		transform.position = pos;
+
+		if (pos.x < -leftAndRightEdge) {
+			speed = Mathf.Abs (speed); 
+		} else if (pos.x > leftAndRightEdge) {
+			speed = -Mathf.Abs (speed);
+		}
+	}
+
+	void FixedUpdate(){
+		if (Random.value < chanceToChangeDirections) {
+			speed *= -1;
+		}
 	}
 }
