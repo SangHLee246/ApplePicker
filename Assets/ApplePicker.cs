69233ce0d0a055dd5bddb86ed5185b6cbe7c41f1
SangHLee246 <akayasha1234@yahoo.com>
2016-06-05 16:31:12 -0400

    HighScore
    Added a HighScore function that keeps track when you lose. You restart
    the round when you run out of basket, but the high school will keep
    track even if you stop the game player function in unity.

--- Assets/ApplePicker.cs
+++ Assets/ApplePicker.cs
@@ -32,5 +32,9 @@ public class ApplePicker : MonoBehaviour {
 		GameObject tBasketGO = basketList [basketIndex];
 		basketList.RemoveAt (basketIndex);
 		Destroy (tBasketGO);
+	
+		if (basketList.Count == 0) {
+			Application.LoadLevel ("_Scene_0");
+		}
 	}
 }
